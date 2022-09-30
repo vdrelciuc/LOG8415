402244e26af73a3f4c3d5b0b7817cc4b0c44fbee
Vlad Drelciuc <vdrelciuc@gmail.com>
2022-09-30 19:49:09 -0400

    Created first and second workloads for benchmarks

--- tp1/main.py
+++ tp1/main.py
@@ -1,12 +1,38 @@
-import json
 import requests
+import time
 
-def call_endpoint_http():
-    url = 'http://ec2-18-212-67-95.compute-1.amazonaws.com/'
+T2_CLUSTER_ELB = 'http://t2-app-load-balancer-865787253.us-east-1.elb.amazonaws.com/'
+M4_CLUSTER_ELB = 'http://m4-app-load-balancer-41265058.us-east-1.elb.amazonaws.com/'
+AWS_ACCESS_KEY_ID = 'XXX'
+AWS_SECRET_ACCESS_KEY = 'YYY'
+AWS_SESSION_TOKEN = 'ZZZ'
+
+def call_endpoint_http(cluster):
     headers = {'content-type': 'application/json'}
-    request = requests.get(url, headers=headers)
-    print(request.status_code)
-    print(request.text)
+    request = requests.get(cluster, headers=headers)
+    # print(request.text) # uncomment to see individual instance_id
 
-call_endpoint_http()
+def run_first_workload():
+    # 1000 GET requests sequentially
+    print('Starting first workload...')
+    for _ in range(1000):
+        call_endpoint_http(T2_CLUSTER_ELB)
+        call_endpoint_http(M4_CLUSTER_ELB)
+    print('Finishing first workload...')
 
+def run_second_workload():
+    # 500 GET requests, then one minute sleep, followed by 1000 GET requests
+    print('Starting second workload...')
+    for _ in range(500):
+        call_endpoint_http(T2_CLUSTER_ELB)
+        call_endpoint_http(M4_CLUSTER_ELB)
+
+    time.sleep(60)
+
+    for _ in range(1000):
+        call_endpoint_http(T2_CLUSTER_ELB)
+        call_endpoint_http(M4_CLUSTER_ELB)
+    print('Finishing second workload...')
+
+run_first_workload()
+run_second_workload()
