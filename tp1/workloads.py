9b77157727c52b6f62c0aa3834ebd601d956b562
Vlad Drelciuc <vlad.drelciuc@polymtl.ca>
2022-10-24 23:06:34 -0400

    Corrected workloads to point to right clusters

--- tp1/workloads.py
+++ tp1/workloads.py
@@ -2,36 +2,36 @@ import requests
 import time
 import threading
 
-def call_endpoint_http(cluster):
+def call_endpoint_http(cluster, path):
     headers = {'content-type': 'application/json'}
-    url = 'http://' + cluster
+    url = 'http://' + cluster + path
     request = requests.get(url, headers=headers, verify=False)
     return request
 
 def run_first_workload(elb_dns):
-    # 1000 GET requests sequentially
+    # 1000 GET requests sequentially to cluster1
     print('Started first workload.')
     for _ in range(1000):
-        call_endpoint_http(elb_dns)
+        call_endpoint_http(elb_dns, '/cluster1')
     print('Finished first workload.')
 
 def run_second_workload(elb_dns):
-    # 500 GET requests, then one minute sleep, followed by 1000 GET requests
+    # 500 GET requests to cluster2, then one minute sleep, followed by 1000 GET requests
     print('Started second workload.')
     for _ in range(500):
-        call_endpoint_http(elb_dns)
+        call_endpoint_http(elb_dns, '/cluster2')
 
     time.sleep(60)
 
     for _ in range(1000):
-        call_endpoint_http(elb_dns)
+        call_endpoint_http(elb_dns, '/cluster2')
     print('Finished second workload.')
 
 def run_workloads(elb):
     elb_dns = elb['LoadBalancers'][0]['DNSName']
 
-    first_workload_thread = threading.Thread(target=run_first_workload, args=(elb_dns))
-    second_workload_thread = threading.Thread(target=run_second_workload, args=(elb_dns))
+    first_workload_thread = threading.Thread(target=run_first_workload, args=[elb_dns])
+    second_workload_thread = threading.Thread(target=run_second_workload, args=[elb_dns])
 
     first_workload_thread.start()
     second_workload_thread.start()
