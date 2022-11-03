9242918a6e2e05201732f6e665ec13212d49499d
William Bourret <46964762+bourret27@users.noreply.github.com>
2022-11-03 15:40:59 -0400

    Created Friendship class. Added a constructor that takes a friend and the relationship as parameters. Also added overide for readFields and write methods from Writable interface.

--- tp2/SocialNetworkProblem/src/com.log8415/Friendship.java
+++ tp2/SocialNetworkProblem/src/com.log8415/Friendship.java
@@ -9,15 +9,23 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class Friendship implements Writable {
-    public Friendship(Text friend, IntWritable relation){
+    Text user = new Text();
+    IntWritable isFriend = new IntWritable();
 
+    public Friendship(Text friend, IntWritable relation){
+        this.user = friend;
+        this.isFriend = relation;
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
+        this.user.readFields(dataInput);
+        this.isFriend.readFields(dataInput);
     }
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
+        this.user.write(dataOutput);
+        this.isFriend.write(dataOutput);
     }
 }
