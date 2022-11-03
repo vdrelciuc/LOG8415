c6262caecc0ef771d1fe7ec092b0f5933cf751db
simon <tran.simon2@gmail.com>
2022-11-03 17:29:08 -0400

    Fixed compilation errors in SocialNetworkProblem.java by adding missing imports

--- tp2/SocialNetworkProblem/src/main/java/SocialNetworkProblem.java
+++ tp2/SocialNetworkProblem/src/main/java/SocialNetworkProblem.java
@@ -1,7 +1,14 @@
 package main.java;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
 
+import org.apache.commons.lang.mutable.MutableInt;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -11,6 +18,7 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
 
 public class SocialNetworkProblem {
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Friendship> {
@@ -19,13 +27,13 @@ public class SocialNetworkProblem {
         final IntWritable isMaybeFriend = new IntWritable(1);
 
         @Override
-        public void map(LongWritable key, Text value, OutputCollector<Text, Friendship> output) {
-            userLine = value.toString().split("\\s");
-            id = new Text[userLine[0]];
-            friends = userLine[1].split(',');
+        public void map(LongWritable key, Text value, OutputCollector<Text, Friendship> output, Reporter reporter) throws IOException {
+            String[] userLine = value.toString().split("\\s");
+            Text id = new Text(userLine[0]);
+            String[] friends = userLine[1].split(",");
 
             for (int i = 0; i < friends.length; i++) {
-                friend = new Text(friends[i]);
+                Text friend = new Text(friends[i]);
                 output.collect(friend, new Friendship(friend, isFriend));
                 for (int j = i + 1; j < friends.length; j++) {
                     Text maybeFriend = new Text(friends[j]);
@@ -36,11 +44,12 @@ public class SocialNetworkProblem {
         }
     }
 
-    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterator<Friendship> values, OutputCollector<Text, Friendship> output) {
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterator<Friendship> values, OutputCollector<Text, Text> output) throws IOException {
             HashMap<String, MutableInt> recommendations = new HashMap<>();
-            ArrayList<String> friendsTracker = new ArrayList();
-            for (Friendship val : values) {
+            ArrayList<String> friendsTracker = new ArrayList<String>();
+            while (values.hasNext()) {
+                final Friendship val = values.next();
                 String person = val.person.toString();
                 if (val.friendship.get() == -1) {
                     friendsTracker.add(person);
@@ -56,24 +65,24 @@ public class SocialNetworkProblem {
                 }
             }
 
-            Comparator<Entry<String, MutuableInt>> valueComparator
-                = new Comparator<Entry<String, MutuableInt>>() {
+            Comparator<Entry<String, MutableInt>> valueComparator
+                = new Comparator<Entry<String, MutableInt>>() {
                     @Override
-                    public int compare(Entry<String, MutuableInt> e1, Entry<String, MutuableInt> e2) {
+                    public int compare(Entry<String, MutableInt> e1, Entry<String, MutableInt> e2) {
                         MutableInt v1 = e1.getValue();
                         MutableInt v2 = e2.getValue();
                         int res = v2.compareTo(v1);
                         if (res == 0)
                         {
-                            return e2.getValue().compreTo(e1.getValue());
+                            return e2.getValue().compareTo(e1.getValue());
                         }
                     }
                 };
                 List<Entry<String, MutableInt>> recommendationsList = new ArrayList<Entry<String, MutableInt>>(recommendations.entrySet());
-                Collections.sort(recommendationsList, valueComparator);
+                recommendationsList.sort(valueComparator);
                 StringBuilder recommendationsText = new StringBuilder();
 
-                for (int i = 0; i < 10 && i < recommendationsList.length; i++) {
+                for (int i = 0; i < 10 && i < recommendationsList.size(); i++) {
                     if (friendsTracker.contains(recommendationsList.get(i).getKey())) {
                         continue;
                     }
