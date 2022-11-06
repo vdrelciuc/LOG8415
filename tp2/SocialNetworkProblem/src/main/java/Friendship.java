e13a11e11f5ec82d26fdd9fca319a2775722f9f7
William Bourret <46964762+bourret27@users.noreply.github.com>
2022-11-06 13:47:53 -0500

    Added documentation for Friendship class.

--- tp2/SocialNetworkProblem/src/main/java/Friendship.java
+++ tp2/SocialNetworkProblem/src/main/java/Friendship.java
@@ -7,31 +7,60 @@ import org.apache.hadoop.io.IntWritable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-
+/*
+ * Friendship class
+ * 
+ * This class represents the relationship a friend/potential friend has with a user.
+ */
 public class Friendship implements Writable {
     Text user = new Text();
     IntWritable isFriend = new IntWritable();
 
+    /*
+     * Default constructor for the friendship class.
+     * Creates a an empty friendship.
+     */
     public Friendship() {
     }
 
+    /*
+     * Parametric constructor for a friendship relation.
+     * 
+     * @param   friend      The friend/potential friend to the user
+     * @param   relation    The relationship the friend/potential friend has with the user (-1 is a friend, 1 is a potential friend) 
+     */
     public Friendship(Text friend, IntWritable relation) {
         this.user = friend;
         this.isFriend = relation;
     }
 
+    /*
+     * Method used to read from an input
+     * 
+     * @param   dataInput   The input containing the data 
+     */
     @Override
     public void readFields(DataInput dataInput) throws IOException {
         this.user.readFields(dataInput);
         this.isFriend.readFields(dataInput);
     }
 
+    /*
+     * Method used to write on an output
+     * 
+     * @param dataOutput    The output where to write the data
+     */
     @Override
     public void write(DataOutput dataOutput) throws IOException {
         this.user.write(dataOutput);
         this.isFriend.write(dataOutput);
     }
 
+    /*
+     * Method to convert Friendship to a string
+     * 
+     * @return String containing the Friendship information
+     */
     @Override
     public String toString() {
         return "Friendship{" +
